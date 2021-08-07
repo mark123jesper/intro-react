f3d2775387f21529f494b23759bfb32231ecdf15
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-08-07 11:05:37 +0800

    uncomment assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -58,11 +58,11 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
+    if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
           placeholder="Add Assignment..." 
@@ -71,7 +71,7 @@ class App extends React.Component {
           title="Student Roster"
         />
       );
-    }*/
+    }
 
     /* Uncomment lines below to render grades*/
     /*if (this.state.buttonClicked === "grades") {
